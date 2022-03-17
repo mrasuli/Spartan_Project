5ac27a61109e64dc575b4a8a1ce236cbaa1a58e0
Mohammad <khalisrasuli@gmai.com>
2022-03-17 13:48:02 +0000

    Added an update on routes and APIs

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+from flask import Flask, request, jsonify
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+flask_object = Flask(__name__)
+
+# http://127.0.0.1:5000
+@flask_object.route('/', methods=['GET'])
+def homepage():
+    return "Welcome to the Spartan's Management System! "
+
+# http://127.0.0.1:5000/spartan/1
+@flask_object.route('/spartan/<spartan_id>', methods=['GET'])
+def spartan_getter(spartan_id):
+    return f"Get the information about spartan's ID: {spartan_id}"
+
+#
+@flask_object.route('/spartan/<spartan_id>', methods=['GET'])
+def spartan_record_getter(spartan_id):
+    spartan_data = jsonify(id=spartan_id, f_n="first_name", l_n="last_name", b_y="birth_year",
+    b_m="birth_month", b_d="birth_day", sp_course="course", sp_stream="stream")
+    return f"Your asking for information about spartan ID: {spartan_id}"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if __name__ == "__main__":
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    flask_object.run()
